cbef35f5dea15f8be892d90e6b276ae61ef20270
Nahid-07 <nahidasif1998@gmail.com>
2022-09-03 00:47:18 +0600

    added manu

--- js/script.js
+++ js/script.js
@@ -2,10 +2,22 @@ const loadData =async()=>{
     try{
         const respose = await fetch('https://openapi.programming-hero.com/api/news/categories')
         const data = await respose.json()
-        console.log(data.data.news_category)
+        return(data.data.news_category)
     }
     catch(error){
         console.log(error);
     }
 }
-loadData()
+// display category on menu
+const loadDataMenu = async()=>{
+    const data =await loadData();
+    // console.log(data)
+    const menuContainer = document.getElementById('menu-container');
+    data.forEach(cate => {
+        // console.log(cate);
+        const li =document.createElement('li');
+        li.innerHTML=`<a onclick="loadNewaData(id)">${cate.category_name}</a>`;
+        menuContainer.appendChild(li)
+    })
+}
+loadDataMenu()
