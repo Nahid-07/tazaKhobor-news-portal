7abf3802cbbafc65b3eb70fe9e02113c1245c870
Nahid-07 <nahidasif1998@gmail.com>
2022-09-03 11:13:35 +0600

    added cards

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const loadDataOnmenu = async() =>{
     try{
-        const response = await fetch('https://openapi.programming-hero.com/api/news/categories')
+        const response = await fetch(`https://openapi.programming-hero.com/api/news/categories`)
         const data = await response.json()
         displayMenu(data.data.news_category)
     }
@@ -9,13 +9,42 @@ const loadDataOnmenu = async() =>{
     }
 }
 const displayMenu = (cate)=>{
-    console.log(cate);
+    // console.log(cate);
     const ul = document.getElementById('menu-container');
     cate.forEach(cates => {
         const li= document.createElement('li');
         li.classList.add('nav-item');
-        li.innerHTML=`<a class="nav-link" href="#">${cates.category_name}</a>`;
+        li.innerHTML=`<a onclick="showALLNews('${cates.category_id}')" class="nav-link" href="#">${cates.category_name}</a>`;
         ul.appendChild(li)
     })
 }
 loadDataOnmenu()
+const showALLNews = (category_id)=>{
+    const url =`https://openapi.programming-hero.com/api/news/category/${category_id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayAllNews(data.data))
+}
+showALLNews()
+const displayAllNews = (news) =>{
+    console.log(news);
+    const contentContainer = document.getElementById('content-container');
+    contentContainer.textContent =''
+    news.forEach(content => {
+     console.log(content)
+     const {thumbnail_url, title, details } = content
+    //  const {img} = author
+     const div = document.createElement('div')
+     div.classList.add('col')
+     div.innerHTML =` 
+     <div class="card">
+     <img src="${thumbnail_url}" class="card-img-top" alt="...">
+     <div class="card-body">
+       <h5 class="card-title">${title}</h5>
+       <p class="card-text">${details.length > 100 ? details.slice(0,100): details}</p>
+     </div>
+   </div>
+     `;
+     contentContainer.appendChild(div)
+    })
+}
