ad340d9acf2c359bbe69d8df5ce56a600e0c09fa
Nahid-07 <nahidasif1998@gmail.com>
2022-09-03 07:44:54 +0600

    added bootstrap

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
-const loadData =async()=>{
+const loadDataOnmenu = async() =>{
     try{
-        const respose = await fetch('https://openapi.programming-hero.com/api/news/categories')
-        const data = await respose.json()
-        return(data.data.news_category)
+        const response = await fetch('https://openapi.programming-hero.com/api/news/categories')
+        const data = await response.json()
+        displayMenu(data.data.news_category)
     }
     catch(error){
-        console.log(error);
+        console.log(error)
     }
 }
-// display category on menu
-const loadDataMenu = async()=>{
-    const data =await loadData();
-    // console.log(data)
-    const menuContainer = document.getElementById('menu-container');
-    data.forEach(cate => {
-        // console.log(cate);
-        const li =document.createElement('li');
-        li.innerHTML=`<a onclick="loadNewaData(id)">${cate.category_name}</a>`;
-        menuContainer.appendChild(li)
+const displayMenu = (cate)=>{
+    console.log(cate);
+    const ul = document.getElementById('menu-container');
+    cate.forEach(cates => {
+        const li= document.createElement('li');
+        li.classList.add('nav-item');
+        li.innerHTML=`<a class="nav-link" href="#">${cates.category_name}</a>`;
+        ul.appendChild(li)
     })
 }
-loadDataMenu()
+loadDataOnmenu()
